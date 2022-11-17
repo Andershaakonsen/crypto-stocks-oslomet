c110c66eb61e3251db8ce0ca88b27c3e877b359e
Andershaakonsen <64076839+Andershaakonsen@users.noreply.github.com>
2022-11-17 18:32:40 +0100

    moved routes into Router.tsx

--- ClientApp/src/main.tsx
+++ ClientApp/src/main.tsx
@@ -1,39 +1,10 @@
 import "./style.scss";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import {
-    createBrowserRouter,
-    RouterProvider,
-    createRoutesFromElements,
-    Route,
-    Link,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import { SWRConfig } from "swr";
 import { ofetch } from "ofetch";
-import DashboardPage from "features/dashboard/DashboardPage";
-import UIPreview from "components/design-system";
-import { Button } from "components";
-
-const Error = () => (
-    <div className="h-screen w-full text-radix-red11 grid place-items-center">
-        <div className="text-center">
-            <span>Something went wrong</span>
-            <Button className="mt-4" color="red" as={Link} to={"/"}>
-                <span>Try again</span>
-            </Button>
-        </div>
-    </div>
-);
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route errorElement={<Error />}>
-            <Route path="/" element={<DashboardPage />} />
-            <Route path="design" element={<UIPreview />} />
-            <Route path="*" element={<div>404</div>} />
-        </Route>
-    )
-);
+import { router } from "Routes";
 
 // Create a global fetcher for SWR using ofetch library (modified fetch wrapper)
 const fetcher = (url: string) => ofetch(url);
