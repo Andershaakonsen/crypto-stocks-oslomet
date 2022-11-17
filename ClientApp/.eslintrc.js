cf9816d5b68767061cbfb5fd1285fa60ae2c1e24
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-11-17 18:26:05 +0100

    chore: add ESLint

---
+++ ClientApp/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: [
+        "prettier",
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+    ],
+    overrides: [],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+        "react/react-in-jsx-scope": "off",
+        "react/no-unescaped-entities": "off",
+        "@typescript-eslint/no-non-null-assertion": "off",
+    },
+};
