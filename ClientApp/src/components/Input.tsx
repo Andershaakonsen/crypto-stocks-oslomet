332172360c5cf07697f08e9282f4561ca1d63d90
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-11-25 19:31:44 +0100

    feat: Add TextField & label

--- ClientApp/src/components/Input.tsx
+++ ClientApp/src/components/Input.tsx
@@ -1,4 +1,5 @@
 import { classed } from "@tw-classed/react";
+import { forwardRef, useId } from "react";
 
 export const Input = classed(
     "input",
@@ -22,6 +23,54 @@ export const Input = classed(
             size: "md",
             color: "secondary",
         },
+    }
+);
+
+export const Label = classed("label", "block mb-1 text-sm font-medium", {
+    variants: {
+        color: {
+            slate: "text-radix-slate11",
+            blue: "text-radix-blue11",
+            green: "text-radix-green11",
+            red: "text-radix-red11",
+        },
+    },
+    defaultVariants: {
+        color: "slate",
+    },
+});
+
+export interface TextFieldProps extends React.ComponentProps<typeof Input> {
+    error?: string;
+    label?: string;
+}
+
+/**
+ * A text field component that supports error states and labels.
+ *
+ * @example
+ * <TextField label="Name" />
+ * With hook form: <TextField label="Name" {...register("name")} />
+ * <TextField label="Name" error="This field is required" />
+ */
+export const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+    ({ error, color, label, ...props }, ref) => {
+        const id = useId();
+        return (
+            <>
+                {label && (
+                    <Label color={error ? "red" : undefined} htmlFor={id}>
+                        {error ? error : label}
+                    </Label>
+                )}
+                <Input
+                    id={id}
+                    color={error ? "error" : color}
+                    ref={ref}
+                    {...props}
+                />
+            </>
+        );
     }
 );
 
--- ClientApp/src/components/index.ts
+++ ClientApp/src/components/index.ts
@@ -1,2 +1,4 @@
 export * from "./Button";
 export * from "./Text";
+export * from "./Input";
+export { default as Details } from "./Details";
