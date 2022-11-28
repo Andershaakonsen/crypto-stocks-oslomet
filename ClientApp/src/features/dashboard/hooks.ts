304ba85414d03b2eb930383e1fc09a9522be8569
Sanna Jammeh <50969683+sannajammeh@users.noreply.github.com>
2022-11-28 17:41:38 +0100

    fix hook

--- ClientApp/src/features/dashboard/hooks.ts
+++ ClientApp/src/features/dashboard/hooks.ts
@@ -1,21 +1,51 @@
 import { useUser } from "features/auth/AuthProvider";
 import { useSWRAuth } from "features/auth/hooks";
-import { Stock } from "types/api";
+import {
+    DepositDTO,
+    ResponseData,
+    Stock,
+    Transaction,
+    Wallet,
+} from "types/api";
+import useSWRMutation from "swr/mutation";
+import { FetchError, ofetch } from "ofetch";
 
 export const useStocks = () => useSWRAuth<Stock[]>("/api/Stocks");
 
 export const useWallets = () => {
     const user = useUser();
-    return useSWRAuth({
+    return useSWRAuth<Wallet[]>({
         url: "/api/Wallets",
-        query: { userId: user?.uid },
+        query: { userId: user?.id },
     });
 };
 
+/**
+ * @example const { data: orders } = useTransactions();
+ */
 export const useOrders = () => {
     const user = useUser();
-    return useSWRAuth({
+    return useSWRAuth<Transaction[]>({
         url: "/api/Orders",
-        query: { userId: user?.uid, limit: 10 },
+        query: { userId: user?.id, limit: 10 },
     });
+};
+
+export const useDeposit = () => {
+    const user = useUser();
+
+    return useSWRMutation<
+        ResponseData<Wallet>,
+        FetchError,
+        string,
+        Pick<DepositDTO, "amount">
+    >("/api/Wallets", (url, { arg }) =>
+        ofetch(url, {
+            method: "POST",
+            body: {
+                ...arg,
+                userId: user?.id,
+            },
+        }).then((d) => d.data)
+    );
 };
